15234d1203eae25f5a79bae27cb2667c3cadea07
Aldrin Sosa <sosaadrin2006@gmail.com>
2026-03-22 11:32:08 +0100

    feat: create eratosthenes.c

---
+++ eratosthenes.c
@@ -0,0 +1,26 @@
+#include "bitarray.h"
+#include <math.h>
+
+void Eratosthenes(bitarray_t array){
+    bitarray_fill(array, 1);
+    bitarray_setbit(array, 0, 0);
+    bitarray_setbit(array, 1, 0);
+    unsigned long sqrt_size = sqrt(bitarray_size(array));
+    unsigned long size = bitarray_size(array);
+    for (unsigned long i = 2; i <= sqrt_size; i++)
+    {
+        //if it isn't 1 it isn't a prime number
+        if (bitarray_getbit(array, i) != 1)
+        {
+            continue;
+        }
+        //use of unsigned long long to prevent overflow
+        //we can start at i*i because al the previous number where already set 
+        //usage of j += i to simulate multiplication 
+        for (unsigned long long j = (unsigned long long)i*i; j < size; j += i)
+        {
+            bitarray_setbit(array, (unsigned long)j, 0);
+        }   
+    }
+    
+}
