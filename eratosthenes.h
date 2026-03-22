834ba37c430a5b8b4d83a3591e5beb7cad318483
Aldrin Sosa <sosaadrin2006@gmail.com>
2026-03-22 12:27:41 +0100

    feat: create eratosthenes.h

---
+++ eratosthenes.h
@@ -0,0 +1,13 @@
+// erastosthenes.c
+// Řešení IJC-DU1, příklad a), 22.3.2026
+// Autor: Aldrin Joseph Sosa Dominguez, FIT
+// Přeloženo: clang 17.0.0
+// Header file of the Eratosthenes algorithm
+
+#ifndef ERATOSTHENES
+#define ERATOSTHENES
+#include "bitarray.h"
+
+void Eratosthenes(bitarray_t array);
+
+#endif
